f8cd8512070afdf8122e2b64458516a7ea9f0f68
Beni Cherniavsky-Paskin <cben@redhat.com>
2018-07-08 13:37:20 +0300

    Split out scrape.sh; wait correctly for healthz/ready
    Waiting for / to return 200 OK was not enough; frequetly getting lots of
    403 right after it, works if re-scraped later.
    Waiting for healthz/ready to return 200 with body "ok" seems reliable.

--- tools/scrape-openshift.sh
+++ tools/scrape-openshift.sh
@@ -1,79 +1,33 @@
 #!/bin/bash
+# Usage: scrape-openshift.sh 3.10.0
 
 set -e -u -o pipefail
 
-VERSION="v$1"
+cd "$(dirname "$(dirname "$0")")"  # run from root of repo.
 
-DIR="$(dirname "$(dirname "$0")")"/openshift-origin-"$VERSION"
+VERSION="v$1"
 
 NAME="origin-$VERSION"
 
+docker-verbose () {
+  echo sudo docker "$@"
+  sudo docker "$@"
+}
+
 # This may fail with "Conflict. The container name "/origin-v1.2.0" is already in use by container ..."
 if docker ps -a | grep "$NAME"; then
-  docker start "$NAME"
+  docker-verbose start "$NAME"
 else
-  sudo docker run -d --name "$NAME" \
+  docker-verbose run -d --name "$NAME" \
        --privileged --pid=host --net=host \
        -v /:/rootfs:ro -v /var/run:/var/run:rw -v /sys:/sys -v /var/lib/docker:/var/lib/docker:rw \
        -v /var/lib/origin/openshift.local.volumes:/var/lib/origin/openshift.local.volumes \
        "docker.io/openshift/origin:$VERSION" start
 fi
 
-rm -rf "$DIR"
-mkdir -p "$DIR"
-cd "$DIR"
-
-URL=https://localhost:8443
-
-scrape () {
-  # Usage: scrape http_path [curl_options...]
-  PTH="$1"
-  shift
-
-  echo -n "Scraping  $DIR/$PTH/index.json  <--  $URL/$PTH  ...  "
-  # "./" prefix allows scrape "" to work.
-  mkdir -p "./$PTH"
-
-  set +e
-  curl --insecure --location "$URL/$PTH" -o "./$PTH/index.json" --verbose "$@" 2> "./$PTH/curl-verbose.txt"
-  status=$?
-  set -e
-
-  echo "$(grep '< HTTP' "./$PTH/curl-verbose.txt" || tail -n1 "./$PTH/curl-verbose.txt")"
-  return $status
-}
-
-echo "Waiting for server..."
-until scrape "" --fail; do
-  sleep 1
-done
-
-scrape version
-scrape version/openshift
-
-echo "Iterating .paths from /"
-for PTH in $(jq --raw-output '.paths[] | ltrimstr("/")' index.json); do
-  scrape "$PTH"
-done
-
-echo "Iterating .versions from api/ and oapi/"
-for GROUP in api oapi; do
-  for APIVER in $(jq --raw-output '.versions[]' "$GROUP/index.json"); do
-    scrape "$GROUP/$APIVER"
-  done
-done
-
-echo "Iterating .groups from apis/"
-scrape "apis"
-for GROUP in $(jq --raw-output '.groups[].name' apis/index.json); do
-  scrape "apis/$GROUP"
-  for APIVER in $(jq --raw-output '.versions[].version' "apis/$GROUP/index.json"); do
-    scrape "apis/$GROUP/$APIVER"
-  done
-done
-
-#docker stop "$NAME"
+env DIR=openshift-origin-"$VERSION" URL=https://localhost:8443 tools/scrape.sh
 
 echo
+docker ps --all --filter=name="$NAME"
 echo "# When done run:"
 echo "docker rm -f '$NAME'"
---
+++ tools/scrape.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Usage: env URL=... DIR=... scrape.sh
+
+set -e -u -o pipefail
+
+rm -rf "$DIR"
+mkdir -p --verbose "$DIR"
+cd "$DIR"
+
+scrape () {
+  # Usage: scrape http_path [curl_options...]
+  PTH="$1"
+  shift
+
+  echo -n "Scraping  $DIR/$PTH/index.json  <--  $URL/$PTH  ...  "
+  # "./" prefix allows scrape "" to work.
+  mkdir -p "./$PTH"
+
+  set +e
+  curl --insecure --location "$URL/$PTH" -o "./$PTH/index.json" --verbose "$@" 2> "./$PTH/curl-verbose.txt"
+  status=$?
+  set -e
+
+  echo "$(grep '< HTTP' "./$PTH/curl-verbose.txt" || tail -n1 "./$PTH/curl-verbose.txt")"
+  return $status
+}
+
+echo "Waiting for server..."
+until scrape healthz/ready --fail && grep ok ./healthz/ready/index.json; do
+  sleep 1
+done
+
+scrape ""
+scrape version
+scrape version/openshift
+
+echo "Iterating .paths from /"
+for PTH in $(jq --raw-output '.paths[] | ltrimstr("/")' index.json); do
+  scrape "$PTH"
+done
+
+# TODO oapi/ is specific to openshift.
+echo "Iterating .versions from api/ and oapi/"
+for GROUP in api oapi; do
+  for APIVER in $(jq --raw-output '.versions[]' "$GROUP/index.json"); do
+    scrape "$GROUP/$APIVER"
+  done
+done
+
+echo "Iterating .groups from apis/"
+scrape "apis"
+for GROUP in $(jq --raw-output '.groups[].name' apis/index.json); do
+  scrape "apis/$GROUP"
+  for APIVER in $(jq --raw-output '.versions[].version' "apis/$GROUP/index.json"); do
+    scrape "apis/$GROUP/$APIVER"
+  done
+done
