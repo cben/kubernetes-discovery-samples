cdcd9ec8db858c8140ea463c02f8c8c884117ee9
Beni Cherniavsky-Paskin <cben@redhat.com>
2018-12-16 12:39:16 +0200

    diff-interesting.sh: ignore more stuff
    `audit-id` header is returned on most or all requests by DigitalOcean.

--- tools/diff-interesting.sh
+++ tools/diff-interesting.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 # Usage:
-#
+#     tools/diff-interesting.sh kubernetes-vA.B.C/ kubernetes-vX.Y.Z/
+# or:
 #     git difftool --dir-diff --extcmd=tools/diff-interesting.sh
 #
 diff --recursive --unified=1 --color \
      --ignore-matching-lines=serverAddress \
+     --ignore-matching-lines='^\* Connected to ' \
+     --ignore-matching-lines='^\*   Trying ' \
+     --ignore-matching-lines='^\*   CAfile:' \
      --ignore-matching-lines='^\*  subject:' \
      --ignore-matching-lines='^\*  start date:' \
      --ignore-matching-lines='^\*  expire date:' \
      --ignore-matching-lines='^\*  issuer:' \
+     --ignore-matching-lines='^\*  SSL certificate verify ' \
      --ignore-matching-lines='^\* Using Stream ID:' \
+     --ignore-matching-lines='^\* Connection #[0-9]* to host ' \
+     --ignore-matching-lines='^\(> \)\?[Hh]ost:' \
      --ignore-matching-lines='^\(< \)\?[Dd]ate:' \
      --ignore-matching-lines='^\(< \)\?[Ll]ast-[Mm]odified' \
      --ignore-matching-lines='^\(> \)\?[Uu]ser-[Aa]gent:' \
      --ignore-matching-lines='^\(< \)\?[Cc]ontent-[Ll]ength:' \
+     --ignore-matching-lines='^\(< \)\?[Aa]udit-[Ii]d:' \
      --ignore-matching-lines='--:--:--' \
      --ignore-matching-lines='[}{] \[[0-9]* bytes data\]' \
      "$@"
